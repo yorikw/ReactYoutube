01baf13fdc183a5bfa81b986caf651cfdde27a59
yvoznyuk <yvoznyuk@gmail.com>
2020-04-03 22:50:21 -0400

    updated packages

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,23 +3,21 @@ import React, {Component} from 'react';
 class SearchBar extends Component{
   constructor(props){
     super(props);
-    this.state = {term: 'cats'}; //set default term
+    this.state = {term: 'COVID-19'}; //set default term
     this.props.onSearchChange(this.state.term);
-  }
-
-  render(){
-    return (
-      <div className="search-bar">
-        <input 
-          value={this.state.term}
-          onChange={event => this.onInputChange(event.target.value)} />
-      </div>
-    );
   }
 
   onInputChange(term){
     this.setState({term});
     this.props.onSearchChange(term);
+  }
+
+  render(){
+    return (
+      <div className="search-bar">
+        <input onChange={event => this.onInputChange(event.target.value)} value={this.state.term} />
+      </div>
+    );
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,19 +10,22 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
+        test: /\.js$/, // include .js files
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
+  //      use: [{
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015', 'stage-1']
+          }
+  //      }]
       }
     ]
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
+  //resolve: {
+  //  extensions: ['', '.js', '.jsx']
+  //},
   devServer: {
     historyApiFallback: true,
     contentBase: './',
